aac764a01ff8bd947c627398e8811fe30cf378ed
Blesy <blesyangulo@gmail.com>
2023-08-09 18:54:04 -0700

    se mueven los types a un scripts especial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,30 +27,11 @@ import V_Spatula from '../components/filters/V_Spatula'
 
 
 // Interfaces y tipos
-type Traits = {
-    [key: string]: number[];
-}
-type Rankings = {
-    platinum: number;
-    gold: number;
-    silver: number;
-    bronze: number;
-    '': number;
-    [key: string]: number;
-};
-type ArrTraits = {
-    quantity: any;
-    arrComparators: number[];
-    quality: string;
-    net: number;
-    name: string;
-}
-interface MyObjType {
-    [key: string]: any;
-}
+import { TraitsType, Rankings, ArrTraits, MyObjType } from './types';
 
+//Constantes globales
 const CHAMPIONS = '/api/champions';
-const TRAITS: Traits = {...Traits}
+const TRAITS: TraitsType = {...Traits}
 
 export default function Home() {
     const initialState = useInitialState(CHAMPIONS)
--- src/app/types.tsx
+++ src/app/types.tsx
@@ -1,5 +1,23 @@
-type TraitsNames = "Darkin" | "Demacia" | "Empress" |"Freljord" |"Ionia" |"Noxus" |"Piltover" |"Shadow Isles" |"Shurima" |"Targon" |"Void" |"Wanderer" |"Yordle" |"Zaun" |"Bastion" |"Bruiser" |"Challenger" |"Deadeye" |"Gunner" 
-|"Invoker" |"Juggernaut" |"Multicaster" |"Redeemer" |"Rogue" |"Slayer" |"Sorcerer" |"Strategist" |"Technogenius";
+type TraitsType = {
+    [key: string]: number[];
+}
+type Rankings = {
+    platinum: number;
+    gold: number;
+    silver: number;
+    bronze: number;
+    '': number;
+    [key: string]: number;
+};
+type ArrTraits = {
+    quantity: any;
+    arrComparators: number[];
+    quality: string;
+    net: number;
+    name: string;
+}
+interface MyObjType {
+    [key: string]: any;
+}
 
-
-export default TraitsNames;
+export type {TraitsType, Rankings, ArrTraits, MyObjType};
